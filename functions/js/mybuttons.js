940e2fe0c1745df8457dbc0eb1319de96283ae51
jofreyswb <jofreyswb@gmail.com>
2015-09-30 17:25:56 +0300

    button
    add 'Paralax' button

---
+++ functions/js/mybuttons.js
@@ -0,0 +1,24 @@
+
+
+(function() {
+    tinymce.create('tinymce.plugins.paralax', {
+        init : function(ed, url) {
+            ed.addButton('paralax', {
+                title : 'Paralax',
+                image : url+'/functions/images/paralax.png',
+                onclick : function() {
+                     ed.selection.setContent('[paralax img=""]' + ed.selection.getContent() + '[/paralax]');
+
+                }
+            });
+
+
+            /* add new button */
+
+        },
+        createControl : function(n, cm) {
+            return null;
+        },
+    });
+    tinymce.PluginManager.add('paralax', tinymce.plugins.paralax);
+})();
--- functions/shortcodes.php
+++ functions/shortcodes.php
@@ -6,7 +6,7 @@
 return'
 <style type="text/css">
 .min_paralax{
-  background: #E2D9DF  url($img ) fixed center center no-repeat ;
+  background: #E2D9DF  url('.$img.') fixed center center no-repeat ;
   height: 100%;
   width: 100%;
 
@@ -17,4 +17,42 @@ return'
 add_shortcode('paralax', 'min_paralax');
 
 
+/*кнопки текстового редактора*/
+function enable_more_buttons($buttons) {
+ $buttons[] = 'hr';
+ $buttons[] = 'sub';
+ $buttons[] = 'sup';
+ $buttons[] = 'fontselect';
+ $buttons[] = 'fontsizeselect';
+ $buttons[] = 'cleanup';
+ $buttons[] = 'styleselect';
+ $buttons[] = 'anchor';
+ return $buttons;
+}
+add_filter("mce_buttons_3", "enable_more_buttons");
+
+/*my button for Paralax
+=========================*/
+
+
+/**/
+function register_button($buttons) {
+   array_push($buttons, "paralax");
+   return $buttons;
+}
+function add_plugin($plugin_array) {
+   $plugin_array['paralax'] = get_bloginfo('template_url').'/functions/js/mybuttons.js';
+   return $plugin_array;
+}
+
+function add_button() {
+   if ( current_user_can('edit_posts') &&  current_user_can('edit_pages') )
+   {
+     add_filter('mce_external_plugins', 'add_plugin');
+     add_filter('mce_buttons', 'register_button');
+   }
+}
+add_action('init', 'add_button');
+/*my button for Paralax
+=========================*/
 ?>
